1fa567ce3d74ee97cb42064cb5d757fdfa5a9ee7
Cezary Naskret <ceznas000@utp.edu.pl>
2019-01-24 21:33:29 +0100

    poczatek projektu

--- Kalkulator/main.cpp
+++ Kalkulator/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include "pierwiastkowanie.h"
 
 using namespace std;
 
 int main()
 {
     int n=0;
-    double a, b;
     cout << "Wybierz opcje" << endl;
+    cout << "5: pierwiastkowanie" << endl;
     cout << "g: zakoncz program" << endl;
-    for (;cin << n;)
+    for (;cin >> n;)
     {
         switch (n)
         {
-        case 1:
+        case 5:
+            Pierwiastkowanie a1;
+            a1.pierwiastkuj();
             break;
         default:
             cout << "Nieznane polecenie." << endl;
---
+++ Kalkulator/pierwiastkowanie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "pierwiastkowanie.h"
+
+using namespace std;
+
+double a;
+
+double Pierwiastkowanie::pierwiastkuj()
+{
+    cout << 0 << endl;
+    return 0;
+}
