0b0bcc42cb8f1e94af10d3d149a4c19bdb83be76
Cezary Naskret <ceznas000@utp.edu.pl>
2019-01-24 22:45:13 +0100

    dzialajace dzielenie

--- Kalkulator/dzielenie.cpp
+++ Kalkulator/dzielenie.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "dzielenie.h"
 
+using namespace std;
+
+double a, b;
+
+double Pierwiastkowanie::pierwiastkuj()
+{
+    cout << "Podaj dwie liczby: ";
+    cin >> a >> b;
+    else cout << a << "/" << b << " wynosi: " << a/b << endl;
+    return 0;
+}
