472a929d63cf0b8a80f50a3ed825b774b23ace91
Izuunia <iza.obrebska@wp.pl>
2019-01-26 00:48:41 +0100

    Dodający

---
+++ Kalkulator/dodawanie.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dodawanie.h"
+
+using namespace std;
+double Dodawanie::dodaj()
+{
+            cout <<"Podaj dwie liczby: ";
+            cin >> a >> b;
+            cout<<a<<"+"<<b<<" wynosi: "<<a+b<<endl;
+            return 0;
+}
