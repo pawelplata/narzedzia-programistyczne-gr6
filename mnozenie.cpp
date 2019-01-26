4cd30f656bed1660447ed3b0b67351fa530414e1
MaxiMeister <mazurek041198@gmail.com>
2019-01-26 18:35:15 +0100

    test

---
+++ mnozenie.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mnozenie.h"
+
+
+
+using namespace std;
+
+double Mnozenie::mnozenie()
+{
+    cout << "Podaj liczbe a: ";
+    cin >> a 
+	cout <<"\nPodaj liczbe b: ";
+	cin >>b;
+    cout  << "\nWynik: " << a*b << endl;
+    return 0;
+}
