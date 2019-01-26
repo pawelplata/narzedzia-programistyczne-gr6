48b5e6bea70c3e6a018229c64955cdf9d56639bd
root <modrak.walter@gmail.com>
2019-01-26 21:21:54 +0100

    poprawa interfejsu cout

--- Kalkulator/dzieleniemodulo.cpp
+++ Kalkulator/dzieleniemodulo.cpp
@@ -7,13 +7,13 @@ using namespace std;
 double DzielenieModulo::dzielmodulo()
 {
 	setlocale(LC_ALL,"PL-pl"); //polskie znaki
-	cout << "Podaj 1-szą liczbę";
+	cout << "Podaj 1-szą liczbę: ";
 	cin >> a;
 	do{
-	cout << "Podaj 2-gą liczbę";
+	cout << "Podaj 2-gą liczbę: ";
 	cin >> b;
-	if(b<=0) cout << "Nie dziel liczby przez 0"; //mianownik różny od zera
+	if(b<=0) cout << "\nNie dziel liczby przez 0!\n"<<endl; //mianownik różny od zera
 	}while(b<=0);
-	cout << "Reszta z dzielenia wynosi: "<<floor(a/b)*b; //podanie wyniku
+	cout << "Reszta z dzielenia wynosi: "<<floor(a/b)*b<<"."<<endl;//podanie wyniku
 	return 0;
 }
