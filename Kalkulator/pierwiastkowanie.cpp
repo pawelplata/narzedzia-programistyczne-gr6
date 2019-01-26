6a119fa8a78cabedf3b37f8c439c06743855f5f4
MaxiMeister <mazurek041198@gmail.com>
2019-01-26 19:41:30 +0100

    proba

--- Kalkulator/pierwiastkowanie.cpp
+++ Kalkulator/pierwiastkowanie.cpp
@@ -8,7 +8,7 @@ double a;
 
 double Pierwiastkowanie::pierwiastkuj()
 {
-    cout << "Podaj liczbe do pierwiastkowania: ";
+    cout << "Podaj liczbe do pierwiaa: ";
     cin >> a;
     if(a<0) cout << "Pierwiastkowac mozna tylko liczby ujemne" << endl;
     else cout << "Pierwiastek z " << a << " wynosi: " << sqrt(a) << endl;
