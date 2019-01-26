a6c0c18ef45ba7481cebcc5e9ff8fb79c8f10d4f
root <modrak.walter@gmail.com>
2019-01-26 21:12:52 +0100

    warunek dla B

--- Kalkulator/dzieleniemodulo.cpp
+++ Kalkulator/dzieleniemodulo.cpp
@@ -8,8 +8,11 @@ double DzielenieModulo::dzielmodulo()
 {
 	cout << "Podaj 1-szą liczbę";
 	cin >> a;
+	do{
 	cout << "Podaj 2-gą liczbę";
 	cin >> b;
+	if(b<=0) cout << "Nie dziel liczby przez 0";
+	}while(b<=0);
 	cout << "Reszta z dzielenia wynosi: "<<floor(a/b)*b;
 	return 0;
 }
