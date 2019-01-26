ac92a9e8db2f327a8a2c26e37d8a93d42450cd11
root <modrak.walter@gmail.com>
2019-01-26 21:07:40 +0100

    dodanie interfejsu

--- Kalkulator/dzieleniemodulo.cpp
+++ Kalkulator/dzieleniemodulo.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 double DzielenieModulo::dzielmodulo()
 {
+	cout << "Podaj 1-szą liczbę";
 	cin >> a;
+	cout << "Podaj 2-gą liczbę";
 	cin >> b;
-	cout << floor(a/b)*b;
+	cout << "Reszta z dzielenia wynosi: "<<floor(a/b)*b;
 	return 0;
 }
