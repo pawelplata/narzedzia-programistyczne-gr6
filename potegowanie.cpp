a2d557fb685ddbc0d473a72e90e15ff9505843ab
igor19999pl <igor19999pl@gmail.com>
2019-01-26 17:07:21 +0100

    potegowanie

--- potegowanie.cpp
+++ potegowanie.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <math.h>
-#include "potegowanie.h"
-
-using namespace std;
-
-double potegowanie::poteguj()
-{
-    cout << "Podaj liczbe, ktora bedziesz potegowac do 2: "<< endl;
-    cin >> a;
-	b=a*a;
-    cout << "Potega liczby " << a << " do 2 wynosi: " << b <<;
-    return 0;
-}
+2
