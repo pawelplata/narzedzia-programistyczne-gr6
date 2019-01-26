cd8ed1436b990647afeae2b9372aca297bad7d8a
Izuunia <iza.obrebska@wp.pl>
2019-01-26 01:14:05 +0100

    popr.dodawanie

--- Kalkulator/dodawanie.cpp
+++ Kalkulator/dodawanie.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 double Dodawanie::dodaj()
 {
-            cout <<"Podaj dwie liczby: ";
+            cout <<"Podaj dwie dowolne  liczby: ";
             cin >> a >> b;
             cout<<a<<"+"<<b<<" wynosi: "<<a+b<<endl;
             return 0;
